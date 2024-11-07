d516598480cc8ae3352e422e38d46e557dcea80e
oppen321 <zj18139624826@gmail.com>
2024-11-07 20:02:14 +0800

    Update 02-prepare_package.sh

--- openwrt/scripts/02-prepare_package.sh
+++ openwrt/scripts/02-prepare_package.sh
@@ -113,7 +113,7 @@ git clone https://$github/UnblockNeteaseMusic/luci-app-unblockneteasemusic packa
 sed -i 's/解除网易云音乐播放限制/网易云音乐解锁/g' package/new/luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
 
 # Theme
-git clone --depth 1 https://$github/oppen321/theme-argon package/new/luci-theme-argon
+git clone https://$github/oppen321/theme-argon package/new/luci-theme-argon
 
 # Change hostname
 sed -i "s/hostname='.*'/hostname='ZeroWrt'/g" package/base-files/files/bin/config_generate
